b35b7b91deaee9f5cc91369b2cb97534c19455e8
Valdeir S <valdeirpsr@hotmail.com.br>
2023-11-17 13:24:05 -0300

    feat: cria layout da página inicial do criador de conteúdo

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>{{ config('app.name') }}</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+
+    <!-- Styles -->
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.ts')
+</head>
+
+<body class="antialiased bg-[#fafafa]">
+    @yield('content')
+
+    <x-comment-dialog />
+    <x-media-viewer />
+</body>
+
+</html>
