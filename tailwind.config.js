29d70467c52e23a02471091c7b9e03a70b9c127c
Valdeir S <valdeirpsr@hotmail.com.br>
2023-11-18 17:58:45 -0300

    refactor: adiciona novas cores ao tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,18 @@ export default {
     extend: {
       container: {
         center: true
+      },
+
+      colors: {
+        approved: '#029ef2',
+        expired: '#ef4444',
       }
     },
   },
   plugins: [],
+  safelist: [
+    'bg-approved',
+    'bg-expired',
+  ]
 }
 
