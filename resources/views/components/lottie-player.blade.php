3d528bc568075b9088c9c4051374ada12db0ceca
Valdeir S <valdeirpsr@hotmail.com.br>
2023-11-18 17:59:14 -0300

    feat: cria componente para renderizar player do lottie

---
+++ resources/views/components/lottie-player.blade.php
@@ -0,0 +1,9 @@
+<dotlottie-player
+    src="{{ Vite::asset("resources/lotties/$name.json") }}"
+    background="transparent"
+    speed="1"
+    direction="1"
+    mode="normal"
+    loop
+    autoplay
+></dotlottie-player>
