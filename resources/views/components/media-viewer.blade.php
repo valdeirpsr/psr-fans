f1487ef98712f1ecff126ec29773d2ecb26dcddb
Valdeir S <valdeirpsr@hotmail.com.br>
2023-11-18 21:59:13 -0300

    refactor: altera dimensões do componente Media Viewer
    Aplica também evento contextmenu para evitar cliques com botão direito
    em imagens do modal. Ademais, corrige opacidade do background e adiciona
    atributos de acessibilidade com role a aria-*

--- resources/views/components/media-viewer.blade.php
+++ resources/views/components/media-viewer.blade.php
@@ -1,11 +1,17 @@
-<div x-data="initMediaViewer">
+<div
+    x-data="initMediaViewer"
+    @contextmenu="(e) => e.preventDefault();"
+>
     <template x-if="isVisible">
         <div
-            class="bg-black bg-opacity-85 fixed inset-0 flex items-center p-4"
+            class="bg-black/90 fixed inset-0 flex items-center justify-center p-4"
+            role="dialog"
+            aria-modal="true"
+            aria-label="Mídia aberta"
             @click="closeModal"
         >
-            {{-- <img class="" src="https://storage.googleapis.com/storage.myfanvip.com.br/prod/user/MU15CFOCSQ/posts/media_1692757281_nathalia_0033.jpg"> --}}
-            <iframe class="h-full w-full" src="https://iframe.mediadelivery.net/embed/173318/b35829ad-6f80-462c-9b7e-66633938b3b2?autoplay=true&loop=false&muted=false&preload=true" loading="lazy" allow="accelerometer;gyroscope;autoplay;encrypted-media;" allowfullscreen="true"></iframe>
+            <img class="max-h-[90vh] max-w-[90vw]" src="https://placehold.co/333x500" alt="" />
+            {{-- <iframe class="h-full w-full max-h-[90vh] max-w-[90vw]" src="https://iframe.mediadelivery.net/embed/173318/b35829ad-6f80-462c-9b7e-66633938b3b2?autoplay=true&loop=false&muted=false&preload=true" loading="lazy" allow="accelerometer;gyroscope;autoplay;encrypted-media;" allowfullscreen="true"></iframe> --}}
         </div>
     </template>
 </div>
