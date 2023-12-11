b5e780fb756891092edec7471eb02991649bd86e
Valdeir S <valdeirpsr@hotmail.com.br>
2023-12-11 13:24:56 -0300

    feat: cria regras de validação para cpf

---
+++ app/Rules/Cpf.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class Cpf implements ValidationRule
+{
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        $c = preg_replace('/\D/', '', $value);
+
+        if (strlen($c) != 11 || preg_match("/^{$c[0]}{11}$/", $c)) {
+            $fail('Invalid CPF');
+            return;
+        }
+
+        for ($s = 10, $n = 0, $i = 0; $s >= 2; $n += $c[$i++] * $s--);
+
+        if ($c[9] != ((($n %= 11) < 2) ? 0 : 11 - $n)) {
+            $fail('Invalid CPF');
+            return;
+        }
+
+        for ($s = 11, $n = 0, $i = 0; $s >= 2; $n += $c[$i++] * $s--);
+
+        if ($c[10] != ((($n %= 11) < 2) ? 0 : 11 - $n)) {
+            $fail('Invalid CPF');
+            return;
+        }
+    }
+}
