e6267bfa3399618cce32a40a342cffd6714ef19d
Valdeir S <valdeirpsr@hotmail.com.br>
2023-12-11 13:25:32 -0300

    refactor: modifica o layout da página de login do Jetstream

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,110 @@
-<x-guest-layout>
-    <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
+@extends('layouts.app') @section('main')
 
-        <x-validation-errors class="mb-4" />
+@if (config('settings.page.login.background'))
+<picture class="h-screen w-screen fixed" x-data="{
+    images: [
+        { height: '915', width: '412', src: '{{ config('settings.page.login.background.sm') }}', maxWidth: '480px' },
+        { height: '915', width: '412', src: '{{ config('settings.page.login.background.md') }}', maxWidth: '800px' },
+        { height: '915', width: '412', src: '{{ config('settings.page.login.background.lg') }}', maxWidth: '1024px' }
+    ]
+}">
+    <template x-for="image in images">
+        <source
+            :media="`(max-width: ${image.maxWidth})`"
+            :srcset="image.src"
+            class="h-full object-cover select-none"
+            fetchpriority="high"
+            loading="eager"
+            :srcset="image.height"
+            :srcset="image.width"
+        />
+    </template>
+    <img
+        src="{{ config('settings.page.login.background.default') }}"
+        alt=""
+        class="h-full object-cover select-none"
+        fetchpriority="high"
+        loading="eager"
+        height="1080"
+        width="1920"
+    />
+</picture>
+@endif
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
-                {{ session('status') }}
-            </div>
-        @endif
+<div
+    x-data="initLoginPage"
+    class="w-screen h-screen grid place-content-center p-4 bg-cover relative"
+>
+    <div
+        class="card rounded-2xl border-2 p-0 overflow-hidden dark:border-gray-700 bg-opacity-90"
+    >
+        <div class="p-4">
+            <x-section-title
+                title="Login"
+                description="Entre com sua conta para acessar o conteúdo disponível"
+            />
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+            <hr class="divide-y-2 my-8 border-gray-300 dark:border-gray-700" />
 
-            <div>
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            </div>
+            @if (session('status'))
+            <x-alert type="success">
+                {{ session("status") }}
+            </x-alert>
+            @endif
 
-            <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
+            <form method="POST" action="{{ route('login') }}" class="space-y-4">
+                @csrf
 
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-checkbox id="remember_me" name="remember" />
-                    <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-                </label>
-            </div>
+                <div>
+                    <x-label
+                        for="input-email"
+                        class="w-full block text-sm pl-4 mb-1 text-gray-600"
+                        >E-mail</x-label
+                    >
+                    <x-input
+                        type="email"
+                        id="input-email"
+                        name="email"
+                        class="form-input border-2 rounded-full h-12 pl-4 w-full placeholder:text-sm"
+                        placeholder="Informe seu e-mail"
+                        autocomplete="username"
+                    />
+                </div>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
+                <div>
+                    <x-label
+                        for="input-password"
+                        class="w-full block text-sm pl-4 mb-1 text-gray-600"
+                        >Senha</x-label
+                    >
+                    <x-input
+                        type="password"
+                        id="input-password"
+                        name="password"
+                        class="form-input border-2 rounded-full h-12 pl-4 w-full placeholder:text-sm"
+                        placeholder="Informe sua senha"
+                        autocomplete="current-password"
+                    />
+                </div>
 
-                <x-button class="ms-4">
-                    {{ __('Log in') }}
-                </x-button>
-            </div>
-        </form>
-    </x-authentication-card>
-</x-guest-layout>
+                <div>
+                    <x-nav-link href="#" class="text-sm float-right"
+                        >Esqueci minha senha</x-nav-link
+                    >
+                    <div class="clear-both"></div>
+                </div>
+
+                <div class="text-center">
+                    <x-validation-errors class="mb-4" />
+                </div>
+
+                <div>
+                    <x-button class="!block w-full">ENTRAR</x-button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<x-media-viewer />
+@endsection
