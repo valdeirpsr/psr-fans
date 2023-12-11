0a0325159dd8030cfa3e3187c597fb8227a7e33c
Valdeir S <valdeirpsr@hotmail.com.br>
2023-12-11 14:19:49 -0300

    refactor: formata código blade com Prettier
    Ademais, adiciona propriedade para acessar o carregamento do banner na
    timeline
    
     - fetchpriority: high
     - loading: eager

--- resources/views/timeline.blade.php
+++ resources/views/timeline.blade.php
@@ -1,43 +1,67 @@
-@extends('layouts.home')
-
-@section('content')
+@extends('layouts.home') @section('content')
 <header class="container sm:max-w-5xl">
-    <img src="https://placehold.co/384x180" class="w-full max-h-80 object-cover" alt="Background" loading="lazy" />
+    <img
+        src="https://placehold.co/384x180"
+        class="w-full max-h-80 object-cover"
+        alt="Background"
+        fetchpriority="high"
+        loading="eager"
+        height="180"
+        width="384"
+    />
 
     <div class="card pt-0">
         <div class="flex items-center space-x-4 -mt-6">
-            <img src="https://placehold.co/96" class="rounded-full border-[2px] border-white" alt="Perfil" height="96" width="96" />
+            <img
+                src="https://placehold.co/96"
+                class="rounded-full border-[2px] border-white"
+                alt="Perfil"
+                height="96"
+                width="96"
+            />
 
             <div>
                 <p class="font-bold">YOUR NAME</p>
-                <p class="text-gray-500 text-sm">104 Fotos | 54 Vídeos</p>
+                <p class="text-gray-500 dark:text-gray-400 text-sm">104 Fotos | 54 Vídeos</p>
             </div>
         </div>
 
-        <p class="mt-3 text-gray-700">
-            Casal que ama aventura, inclusive no sexo! Muita experiência no menage com meninas e aqui vamos mostrar um pouquinho de conteúdo nosso e com elas. Menos hipocrisia e mais sexo, por favor!
+        <p class="mt-3 text-gray-700 dark:text-gray-400">
+            Casal que ama aventura, inclusive no sexo! Muita experiência no
+            menage com meninas e aqui vamos mostrar um pouquinho de conteúdo
+            nosso e com elas. Menos hipocrisia e mais sexo, por favor!
         </p>
     </div>
 
-    <div class="card" x-data={}>
-        <p class="text-sm text-gray-500">Assine</p>
+    <div class="card" x-data="{}">
+        <p class="text-sm text-gray-500 dark:text-gray-400">Assine</p>
 
         <div class="mt-3 text-center">
-            <x-inputs.button type="button" @click="$dispatch('open-subscribe-modal')">
+            <x-inputs.button
+                type="button"
+                @click="$dispatch('open-subscribe-modal')"
+            >
                 ASSINE O CONTÉUDO POR R$49,99
             </x-inputs.button>
         </div>
     </div>
 
-    {{-- <div class="card" x-data={}>
-        <p class="text-sm text-gray-500">Sua assinatura expira em 31 dez 2023</p>
+    {{--
+    <div class="card" x-data="{}">
+        <p class="text-sm text-gray-500 dark:text-gray-400">
+            Sua assinatura expira em 31 dez 2023
+        </p>
 
         <div class="mt-3 text-center">
-            <x-inputs.button type="button" @click="$dispatch('open-subscribe-modal')">
+            <x-inputs.button
+                type="button"
+                @click="$dispatch('open-subscribe-modal')"
+            >
                 RENOVAR
             </x-inputs.button>
         </div>
-    </div> --}}
+    </div>
+    --}}
 </header>
 
 <main class="mt-3">
