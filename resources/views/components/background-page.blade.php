11631a60472fca222d48f2bb7e115f3c29600b20
Valdeir S <valdeirpsr@hotmail.com.br>
2023-12-11 14:17:58 -0300

    refactor: remove click com botão direito do mouse sobre background-page

--- resources/views/components/background-page.blade.php
+++ resources/views/components/background-page.blade.php
@@ -2,13 +2,17 @@
     'page'
 ])
 @if (config('settings.page.' . $page . '.background'))
-<picture class="h-screen w-screen fixed" x-data="{
-    images: [
-        { height: '915', width: '412', src: '{{ config('settings.page.' . $page . '.background.sm') }}', maxWidth: '480px' },
-        { height: '915', width: '412', src: '{{ config('settings.page.' . $page . '.background.md') }}', maxWidth: '800px' },
-        { height: '915', width: '412', src: '{{ config('settings.page.' . $page . '.background.lg') }}', maxWidth: '1024px' }
-    ]
-}">
+<picture
+    class="h-screen w-screen fixed"
+    x-data="{
+        images: [
+            { height: '915', width: '412', src: '{{ config('settings.page.' . $page . '.background.sm') }}', maxWidth: '480px' },
+            { height: '915', width: '412', src: '{{ config('settings.page.' . $page . '.background.md') }}', maxWidth: '800px' },
+            { height: '915', width: '412', src: '{{ config('settings.page.' . $page . '.background.lg') }}', maxWidth: '1024px' }
+        ]
+    }"
+    @contextmenu="(e) => e.preventDefault()"
+>
     <img
         src="{{ config('settings.page.' . $page . '.background.default') }}"
         alt=""
