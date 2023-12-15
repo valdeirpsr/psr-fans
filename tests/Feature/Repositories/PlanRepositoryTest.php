3d47e0976fce54cde270f3bc1c748f8ee3d3052a
Valdeir S <valdeirpsr@hotmail.com.br>
2023-12-15 13:14:52 -0300

    tests: adiciona testes PlanRepository e PlanService

--- tests/Feature/Repositories/PlanRepositoryTest.php
+++ tests/Feature/Repositories/PlanRepositoryTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Repositories;
 
 use App\Models\Plan;
 use App\Repositories\PlanRepository;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -26,6 +27,34 @@ class PlanRepositoryTest extends TestCase
         $repository = new PlanRepository;
         $result = $repository->list();
 
-        $this->assertEquals(10, $result->count());
+        $this->assertCount(10, $result);
+    }
+
+    public function test_o_metodo_get_deve_retornar_um_plano_de_assinatura_habilitado_de_acordo_com_o_id(): void
+    {
+        Plan::factory(10)->create();
+
+        Plan::factory()->enabled()->createOne([
+            'name' => 'Special',
+            'id' => 1000,
+        ]);
+
+        $repository = new PlanRepository;
+        $result = $repository->get(1000);
+
+        $this->assertEquals('Special', $result['name']);
+    }
+
+    public function test_se_o_plano_capturado_estiver_desabilitado_devera_retornar_uma_exception(): void
+    {
+        $this->expectException(ModelNotFoundException::class);
+
+        Plan::factory()->createOne([
+            'id' => 1000,
+            'status' => false,
+        ]);
+
+        $repository = new PlanRepository;
+        $repository->get(1000);
     }
 }
--- tests/Feature/Services/PlanServiceTest.php
+++ tests/Feature/Services/PlanServiceTest.php
@@ -2,9 +2,21 @@
 
 namespace Tests\Feature\Services;
 
+use App\Models\Plan;
+use App\Repositories\PlanRepository;
+use App\Services\PlanService;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Session;
+use Mockery;
+use Mockery\MockInterface;
 use Tests\TestCase;
+
+use function PHPUnit\Framework\assertEquals;
+use function PHPUnit\Framework\assertNull;;
+use function PHPUnit\Framework\assertTrue;
 
 class PlanServiceTest extends TestCase
 {
@@ -14,5 +26,61 @@ class PlanServiceTest extends TestCase
     public function test_example(): void
     {
         $this->markTestSkipped();
+    }
+
+    /**
+     * Se a sessão `plan` não existir, deve-se retornar null
+     */
+    public function test_valida_o_metodo_getBySession_com_uma_sessao_inexistente(): void
+    {
+        $repositoryMock = Mockery::mock(PlanRepository::class);
+
+        $service = new PlanService($repositoryMock);
+        $result = $service->getBySession();
+
+        assertNull($result);
+    }
+
+    public function test_valida_o_metodo_getBySession_com_uma_sessao_existente_porem_invalida(): void
+    {
+        $repositoryMock = Mockery::mock(PlanRepository::class);
+
+        Session::put('plan', null);
+
+        $service = new PlanService($repositoryMock);
+        $result = $service->getBySession();
+
+        assertNull($result);
+    }
+
+    public function test_valida_o_metodo_getBySession_com_uma_sessao_valida(): void
+    {
+        $repositoryMock = Mockery::mock(PlanRepository::class, function (MockInterface $mock) {
+            $mock->shouldReceive('get')->once()->with(1)->andReturn(
+                Plan::factory()->enabled()->make()
+            );
+        });
+
+        Session::put('plan', 1);
+
+        $service = new PlanService($repositoryMock);
+        /** @var Plan $result */
+        $result = $service->getBySession();
+
+        assertTrue($result['status']);
+    }
+
+    public function test_valida_o_metodo_getBySession_com_uma_sessao_valida_e_um_plano_desabilitado(): void
+    {
+        $repositoryMock = Mockery::mock(PlanRepository::class, function (MockInterface $mock) {
+            $mock->shouldReceive('get')->once()->with(1)->andThrow(ModelNotFoundException::class);
+        });
+
+        Session::put('plan', 1);
+
+        $service = new PlanService($repositoryMock);
+        $result = $service->getBySession();
+
+        assertNull($result);
     }
 }
