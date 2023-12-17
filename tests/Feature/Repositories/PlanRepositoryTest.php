e50a2dd1b81b53cb667a940c16878bb7cdeb9055
Valdeir S <valdeirpsr@hotmail.com.br>
2023-12-17 10:22:05 -0300

    tests: adiciona testes ao PlanRepositoryTest

--- tests/Feature/Repositories/PlanRepositoryTest.php
+++ tests/Feature/Repositories/PlanRepositoryTest.php
@@ -2,8 +2,12 @@
 
 use App\Models\Plan;
 use App\Repositories\PlanRepository;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use function PHPUnit\Framework\assertCount;
+
+uses(RefreshDatabase::class);
 
 it('Ao listar os planos, apenas os publicados devem ser retornados', function () {
     Plan::factory()->draft()->createOneQuietly();
@@ -14,3 +18,8 @@ it('Ao listar os planos, apenas os publicados devem ser retornados', function ()
 
     assertCount(1, $result);
 });
+
+it('Ao capturar um plano, se esse não existir, deve-se lançar uma exception', function () {
+    $repository = new PlanRepository;
+    $repository->findById(0);
+})->throws(ModelNotFoundException::class);
