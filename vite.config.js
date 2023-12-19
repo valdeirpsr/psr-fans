736cac17b3c7348291d80168838d3df603fb82e6
Valdeir S <valdeirpsr@hotmail.com.br>
2023-12-19 11:23:59 -0300

    build: adiciona dependência do MercadoPago
    Será usado para pagamentos via PIX

--- vite.config.js
+++ vite.config.js
@@ -22,6 +22,7 @@ export default defineConfig({
             refresh: [
                 ...refreshPaths,
                 'app/Livewire/**',
+                'views/**',
             ],
         }),
     ],
